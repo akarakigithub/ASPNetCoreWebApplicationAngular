ffd4f246b6a28bc625db187aff792d2e036a13fa
akarakigithub <67332091+akarakigithub@users.noreply.github.com>
2020-10-17 10:20:07 -0700

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace ASPNetCoreWebApplicationAngular.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Hot and Mild", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Hot and Mild", "OK", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
